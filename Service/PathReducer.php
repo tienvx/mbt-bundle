83dc5a88fa868b51160bd0f9cb434d12b8a30c62
tien.xuan.vo <tien.xuan.vo@gmail.com>
2017-11-12 21:11:19 +0700

    Remove edges if first vertex and second vertex are the same. Try until distance = 0

--- Service/PathReducer.php
+++ Service/PathReducer.php
@@ -2,6 +2,7 @@
 
 namespace Tienvx\Bundle\MbtBundle\Service;
 
+use Fhaculty\Graph\Vertex;
 use Graphp\Algorithms\ShortestPath\Dijkstra;
 use Tienvx\Bundle\MbtBundle\Graph\Path;
 use Tienvx\Bundle\MbtBundle\Model\Model;
@@ -22,18 +23,20 @@ class PathReducer
     {
         $distance = $path->countVertices() - 1;
 
-        while ($distance > 1) {
+        while ($distance > 0) {
             for ($i = 0; $i < $path->countVertices() - 1; $i++) {
                 for ($j = $path->countVertices() - 1; $j > $i; $j--) {
                     if (($j - $i) === $distance) {
                         $newPath = $this->getNewPath($path, $i, $j);
                         // Make sure new path walkable.
-                        if ($this->runner->canWalk($newPath, $model)) {
+                        if ($newPath instanceof Path && $this->runner->canWalk($newPath, $model)) {
                             try {
                                 $this->runner->run($newPath, $model);
                             } catch (\Throwable $newThrowable) {
                                 if ($newThrowable->getMessage() === $throwable->getMessage()) {
-                                    return $newPath;
+                                    $path = $newPath;
+                                    $distance = $path->countVertices() - 1;
+                                    break 2;
                                 }
                             }
                         }
@@ -47,35 +50,48 @@ class PathReducer
         return $path;
     }
 
-    protected function getNewPath(Path $path, $firstVertexIndex, $secondVertexIndex): Path
+    protected function getNewPath(Path $path, $firstVertexIndex, $secondVertexIndex): ?Path
     {
         $vertices = $path->getVertices();
         $edges = $path->getEdges();
         $allData = $path->getAllData();
+        /** @var Vertex $firstVertex */
         $firstVertex = $vertices[$firstVertexIndex];
+        /** @var Vertex $secondVertex */
         $secondVertex = $vertices[$secondVertexIndex];
 
-        // Remove any edges between first vertex and second vertex.
-        $algorithm = new Dijkstra($firstVertex);
+        if ($secondVertexIndex - $firstVertexIndex === 1 && $firstVertex->getId() !== $secondVertex->getId()) {
+            // 2 vertices are near in the path, it does not worth to reduce the path.
+            return null;
+        }
+
         $beginEdges = [];
-        $middleEdges = $algorithm->getEdgesTo($secondVertex)->getVector();
         $endEdges = [];
         $beginData = [];
-        $middleData = array_fill(0, count($middleEdges), null);
         $endData = [];
         foreach ($edges as $index => $edge) {
             if ($index < $firstVertexIndex) {
                 $beginEdges[] = $edge;
                 $beginData[] = $allData[$index];
             }
-            elseif ($index < $secondVertexIndex) {
-                // Middle edges are replaced by algorithm.
-            }
-            else {
+            elseif ($index >= $secondVertexIndex) {
                 $endEdges[] = $edge;
                 $endData[] = $allData[$index];
             }
         }
+
+        if ($firstVertex->getId() !== $secondVertex->getId()) {
+            // Replace all edges between first vertex and second vertex by algorithm.
+            $algorithm = new Dijkstra($firstVertex);
+            $middleEdges = $algorithm->getEdgesTo($secondVertex)->getVector();
+            $middleData = array_fill(0, count($middleEdges), null);
+        }
+        else {
+            // Remove all edges between first vertex and second vertex.
+            $middleEdges = [];
+            $middleData = [];
+        }
+
         $edges = array_merge($beginEdges, $middleEdges, $endEdges);
         $newPath = Path::factoryFromEdges($edges, $vertices[0]);
         $allData = array_merge($beginData, $middleData, $endData);
