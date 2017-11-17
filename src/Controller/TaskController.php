a2c50659b139bcfc63c5c0ba11b178f6f41ae511
tien.xuan.vo <tien.xuan.vo@gmail.com>
2017-11-17 10:58:48 +0700

    Use logic path to templates

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -8,11 +8,11 @@ class TaskController extends Controller
 {
     public function startAction()
     {
-        return $this->render('TienvxMbtBundle:Task:start.html.twig');
+        return $this->render('@TienvxMbt/Task/start.html.twig');
     }
 
     public function stopAction()
     {
-        return $this->render('TienvxMbtBundle:Task:stop.html.twig');
+        return $this->render('@TienvxMbt/Task/stop.html.twig');
     }
 }
