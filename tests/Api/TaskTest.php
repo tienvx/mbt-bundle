ee67019904821af0e97086679398eee82383e4f8
tien.xuan.vo <tien.xuan.vo@gmail.com>
2018-01-01 23:55:14 +0700

    Make task api test cases better, add bug api test cases

--- tests/Api/TaskTest.php
+++ tests/Api/TaskTest.php
@@ -7,10 +7,46 @@ class TaskTest extends AbstractApiTestCase
     public function testGetTasks()
     {
         $response = $this->makeApiRequest('GET', '/mbt/tasks');
-        $this->assertEquals('[{"id":1,"title":"Task 1","model":"shopping_cart","algorithm":"random","progress":0,"status":"not-started","bugs":["\/mbt\/bugs\/1","\/mbt\/bugs\/2"]},{"id":2,"title":"Task 2","model":"shopping_cart","algorithm":"random","progress":64,"status":"in-progress","bugs":[]},{"id":3,"title":"Task 3","model":"shopping_cart","algorithm":"random","progress":100,"status":"completed","bugs":["\/mbt\/bugs\/3"]}]', $response->getContent());
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals('application/json; charset=utf-8', $response->headers->get('Content-Type'));
+        $this->assertJsonStringEqualsJsonString('
+        [
+          {
+            "id": 1,
+            "title": "Task 1",
+            "model": "shopping_cart",
+            "algorithm": "random",
+            "progress": 0,
+            "status": "not-started",
+            "bugs": [
+              "/mbt/bugs/1",
+              "/mbt/bugs/2"
+            ]
+          },
+          {
+            "id": 2,
+            "title": "Task 2",
+            "model": "shopping_cart",
+            "algorithm": "random",
+            "progress": 64,
+            "status": "in-progress",
+            "bugs": []
+          },
+          {
+            "id": 3,
+            "title": "Task 3",
+            "model": "shopping_cart",
+            "algorithm": "random",
+            "progress": 100,
+            "status": "completed",
+            "bugs": [
+              "/mbt/bugs/3"
+            ]
+          }
+        ]', $response->getContent());
     }
 
-    public function testCreateTasks()
+    public function testCreateTask()
     {
         $response = $this->makeApiRequest('POST', '/mbt/tasks', '
         {
@@ -21,6 +57,52 @@ class TaskTest extends AbstractApiTestCase
           "status": "not-started"
         }');
 
-        $this->assertEquals('{"id":4,"title":"Test shopping cart","model":"shopping_cart","algorithm":"random","progress":0,"status":"not-started","bugs":[]}', $response->getContent());
+        $this->assertEquals(201, $response->getStatusCode());
+        $this->assertEquals('application/json; charset=utf-8', $response->headers->get('Content-Type'));
+        $this->assertJsonStringEqualsJsonString('
+        {
+          "id": 4,
+          "title": "Test shopping cart",
+          "model": "shopping_cart",
+          "algorithm": "random",
+          "progress": 0,
+          "status": "not-started",
+          "bugs": []
+        }', $response->getContent());
+    }
+
+    public function testCreateInvalidTask()
+    {
+        $response = $this->makeApiRequest('POST', '/mbt/tasks', '
+        {
+          "title": "Test shopping cart",
+          "model": "shopping_cart",
+          "algorithm": "all-transitions",
+          "progress": 111,
+          "status": "not-supported"
+        }');
+
+        $this->assertEquals(400, $response->getStatusCode());
+        $this->assertEquals('application/problem+json; charset=utf-8', $response->headers->get('Content-Type'));
+        $this->assertJsonStringEqualsJsonString('
+        {
+          "type": "https://tools.ietf.org/html/rfc2616#section-10",
+          "title": "An error occurred",
+          "detail": "algorithm: The value you selected is not a valid choice.\nprogress: This value should be 100 or less.\nstatus: The value you selected is not a valid choice.",
+          "violations": [
+            {
+              "propertyPath": "algorithm",
+              "message": "The value you selected is not a valid choice."
+            },
+            {
+              "propertyPath": "progress",
+              "message": "This value should be 100 or less."
+            },
+            {
+              "propertyPath": "status",
+              "message": "The value you selected is not a valid choice."
+            }
+          ]
+        }', $response->getContent());
     }
 }
