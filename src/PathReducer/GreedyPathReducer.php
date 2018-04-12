13dfea4d308e5fe562f2b21baeb4e3c41e027bb6
tien.xuan.vo <tien.xuan.vo@gmail.com>
2018-04-12 22:58:39 +0700

    Improve greedy path reducer

--- src/PathReducer/GreedyPathReducer.php
+++ src/PathReducer/GreedyPathReducer.php
@@ -14,38 +14,33 @@ class GreedyPathReducer extends AbstractPathReducer
 
         while ($distance > 0) {
             $pairsByDistance = [];
-            for ($i = 0; $i < $distance; $i++) {
-                for ($j = $distance; $j > $i; $j--) {
+            for ($i = 0; $i < $path->countVertices() - 1; $i++) {
+                $j = $i + $distance;
+                if ($j < $path->countVertices()) {
+                    $pairsByDistance[$j - $i] = $pairsByDistance[$j - $i] ?? [];
                     if ($path->getVertexAt($i)->getId() === $path->getVertexAt($j)->getId()) {
-                        $pairsByDistance[$j - $i][] = [$i, $j];
+                        array_unshift($pairsByDistance[$j - $i], [$i, $j]);
+                    }
+                    elseif ($path->getVertexAt($i)->getId() !== $path->getVertexAt($j)->getId() && $distance > 1) {
+                        array_push($pairsByDistance[$j - $i], [$i, $j]);
                     }
                 }
             }
             krsort($pairsByDistance);
-            $pairs = [];
-            foreach ($pairsByDistance as $array) {
-                $pairs = array_merge($pairs, $array);
-            }
-            for ($i = 0; $i < $distance; $i++) {
-                for ($j = $distance; $j > $i; $j--) {
-                    // Ignore 2 vertices are near in the path, it does not worth to reduce the path.
-                    if ($path->getVertexAt($i)->getId() !== $path->getVertexAt($j)->getId() && $distance > 1 && ($j - $i) === $distance) {
-                        $pairs[] = [$i, $j];
-                    }
-                }
-            }
-            foreach ($pairs as $pair) {
-                list($i, $j) = $pair;
-                $newPath = $this->getNewPath($path, $i, $j);
-                // Make sure new path walkable.
-                if ($this->runner->canWalk($newPath, $model) && $newPath->countVertices() < $path->countVertices()) {
-                    try {
-                        $this->runner->run($newPath, $model);
-                    } catch (Throwable $newThrowable) {
-                        if ($newThrowable->getMessage() === $throwable->getMessage()) {
-                            $path = $newPath;
-                            $distance = $path->countVertices() - 1;
-                            break;
+            foreach ($pairsByDistance as $pairs) {
+                foreach ($pairs as $pair) {
+                    list($i, $j) = $pair;
+                    $newPath = $this->getNewPath($path, $i, $j);
+                    // Make sure new path walkable.
+                    if ($this->runner->canWalk($newPath, $model) && $newPath->countVertices() < $path->countVertices()) {
+                        try {
+                            $this->runner->run($newPath, $model);
+                        } catch (Throwable $newThrowable) {
+                            if ($newThrowable->getMessage() === $throwable->getMessage()) {
+                                $path = $newPath;
+                                $distance = $path->countVertices() - 1;
+                                break;
+                            }
                         }
                     }
                 }
