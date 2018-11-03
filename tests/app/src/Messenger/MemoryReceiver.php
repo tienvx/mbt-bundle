283c69352b8b2f905349ab71bb3e7e611bc8adcc
tien.xuan.vo <tien.xuan.vo@gmail.com>
2018-11-03 17:30:03 +0700

    Use memory transport, remove filesystem transport

---
+++ tests/app/src/Messenger/MemoryReceiver.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Messenger;
+
+use Symfony\Component\Messenger\Transport\ReceiverInterface;
+
+class MemoryReceiver implements ReceiverInterface
+{
+    private $connection;
+
+    public function __construct(Connection $connection)
+    {
+        $this->connection = $connection;
+    }
+
+    public function receive(callable $handler): void
+    {
+        $message = $this->connection->get();
+        $handler($message);
+    }
+
+    public function stop(): void
+    {
+    }
+}
