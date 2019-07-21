6343eb6bd121c8eaaa6c37de133af91b1496d1bc
tien.xuan.vo <tien.xuan.vo@gmail.com>
2019-07-21 19:24:15 +0700

    Move bootstrap to config

---
+++ tests/app/config/bundles.php
@@ -0,0 +1,17 @@
+<?php
+
+use Doctrine\Bundle\DoctrineBundle\DoctrineBundle;
+use League\FlysystemBundle\FlysystemBundle;
+use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
+use Symfony\Bundle\MakerBundle\MakerBundle;
+use Symfony\Bundle\SecurityBundle\SecurityBundle;
+use Tienvx\Bundle\MbtBundle\TienvxMbtBundle;
+
+return [
+    SecurityBundle::class => ['all' => true],
+    FrameworkBundle::class => ['all' => true],
+    DoctrineBundle::class => ['all' => true],
+    MakerBundle::class => ['all' => true],
+    FlysystemBundle::class => ['all' => true],
+    TienvxMbtBundle::class => ['all' => true],
+];
