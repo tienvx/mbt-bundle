536f3da76dd2ca88696bdd41623796d892fb326d
tien.xuan.vo <tien.xuan.vo@gmail.com>
2020-11-09 23:45:17 +0700

    Fix missing service's argument

--- src/Resources/config/services.php
+++ src/Resources/config/services.php
@@ -76,47 +76,50 @@ return static function (ContainerConfigurator $container): void {
 
         ->set(EntitySubscriber::class)
             ->tag('doctrine.event_subscriber')
+            ->args([
+                new Reference(MessageBusInterface::class),
+            ])
 
         ->set(GeneratorManager::class)
         ->set(RandomGenerator::class)
 
         ->set(ExecuteTaskMessageHandler::class)
-        ->args([
-            new Reference(GeneratorManager::class),
-            new Reference(EntityManagerInterface::class),
-            new Reference(StepsRunnerInterface::class),
-            new Reference(ConfigLoaderInterface::class),
-            new Reference(TaskProgressInterface::class),
-            new Reference(BugHelperInterface::class),
-        ])
+            ->args([
+                new Reference(GeneratorManager::class),
+                new Reference(EntityManagerInterface::class),
+                new Reference(StepsRunnerInterface::class),
+                new Reference(ConfigLoaderInterface::class),
+                new Reference(TaskProgressInterface::class),
+                new Reference(BugHelperInterface::class),
+            ])
 
         ->set(ReduceBugMessageHandler::class)
-        ->args([
-            new Reference(ReducerManager::class),
-            new Reference(EntityManagerInterface::class),
-            new Reference(MessageBusInterface::class),
-            new Reference(ConfigLoaderInterface::class),
-            new Reference(BugProgressInterface::class),
-        ])
+            ->args([
+                new Reference(ReducerManager::class),
+                new Reference(EntityManagerInterface::class),
+                new Reference(MessageBusInterface::class),
+                new Reference(ConfigLoaderInterface::class),
+                new Reference(BugProgressInterface::class),
+            ])
 
         ->set(ReduceStepsMessageHandler::class)
-        ->args([
-            new Reference(ReducerManager::class),
-            new Reference(EntityManagerInterface::class),
-            new Reference(MessageBusInterface::class),
-            new Reference(ConfigLoaderInterface::class),
-            new Reference(BugProgressInterface::class),
-        ])
+            ->args([
+                new Reference(ReducerManager::class),
+                new Reference(EntityManagerInterface::class),
+                new Reference(MessageBusInterface::class),
+                new Reference(ConfigLoaderInterface::class),
+                new Reference(BugProgressInterface::class),
+            ])
 
         ->set(ReportBugMessageHandler::class)
-        ->args([
-            new Reference(EntityManagerInterface::class),
-            new Reference(NotifierInterface::class),
-            new Reference(ConfigLoaderInterface::class),
-            new Reference(BugSubscriberInterface::class),
-            new Reference(BugHelperInterface::class),
-            new Reference(TranslatorInterface::class),
-        ])
+            ->args([
+                new Reference(EntityManagerInterface::class),
+                new Reference(NotifierInterface::class),
+                new Reference(ConfigLoaderInterface::class),
+                new Reference(BugSubscriberInterface::class),
+                new Reference(BugHelperInterface::class),
+                new Reference(TranslatorInterface::class),
+            ])
 
         ->set(ReducerManager::class)
         ->set(RandomDispatcher::class)
@@ -156,7 +159,7 @@ return static function (ContainerConfigurator $container): void {
             ->args([
                 new Reference(ExpressionLanguage::class),
             ])
-        ->alias(ExpressionEvaluatorInterface::class, ExpressionLanguageEvaluator::class)
+            ->alias(ExpressionEvaluatorInterface::class, ExpressionLanguageEvaluator::class)
 
         ->set(ColorfulFactory::class)
             ->args([
@@ -171,14 +174,14 @@ return static function (ContainerConfigurator $container): void {
                 Token::class,
                 Marking::class,
             ])
-        ->alias(ColorfulFactoryInterface::class, ColorfulFactory::class)
+            ->alias(ColorfulFactoryInterface::class, ColorfulFactory::class)
 
         ->set(GuardedTransitionService::class)
             ->args([
                 new Reference(ColorfulFactoryInterface::class),
                 new Reference(ExpressionEvaluatorInterface::class),
             ])
-        ->alias(GuardedTransitionServiceInterface::class, GuardedTransitionService::class)
+            ->alias(GuardedTransitionServiceInterface::class, GuardedTransitionService::class)
 
         ->set(SingleColorPetrinetBuilder::class)
             ->args([
@@ -196,13 +199,13 @@ return static function (ContainerConfigurator $container): void {
                 new Reference(ConfigLoaderInterface::class),
                 new Reference(TranslatorInterface::class),
             ])
-        ->alias(BugHelperInterface::class, BugHelper::class)
+            ->alias(BugHelperInterface::class, BugHelper::class)
 
         ->set(BugProgress::class)
             ->args([
                 new Reference(EntityManagerInterface::class),
             ])
-        ->alias(BugProgressInterface::class, BugProgress::class)
+            ->alias(BugProgressInterface::class, BugProgress::class)
 
         ->set(BugSubscriberInterface::class)
         ->set(ConfigLoaderInterface::class)
@@ -213,37 +216,37 @@ return static function (ContainerConfigurator $container): void {
             ->args([
                 new Reference(ConfigLoaderInterface::class),
             ])
-        ->alias(SeleniumInterface::class, Selenium::class)
+            ->alias(SeleniumInterface::class, Selenium::class)
 
         ->set(ShortestPathStepsBuilder::class)
             ->args([
                 new Reference(ShortestPathStrategyInterface::class),
             ])
-        ->alias(StepsBuilderInterface::class, ShortestPathStepsBuilder::class)
+            ->alias(StepsBuilderInterface::class, ShortestPathStepsBuilder::class)
 
         ->set(AStarStrategy::class)
             ->args([
                 new Reference(GuardedTransitionServiceInterface::class),
             ])
-        ->alias(ShortestPathStrategyInterface::class, AStarStrategy::class)
+            ->alias(ShortestPathStrategyInterface::class, AStarStrategy::class)
 
         ->set(StepRunner::class)
             ->args([
                 new Reference(SeleniumInterface::class),
             ])
-        ->alias(StepRunnerInterface::class, StepRunner::class)
+            ->alias(StepRunnerInterface::class, StepRunner::class)
 
         ->set(StepsRunner::class)
             ->args([
                 new Reference(GuardedTransitionServiceInterface::class),
                 new Reference(StepRunnerInterface::class),
             ])
-        ->alias(StepsRunnerInterface::class, StepsRunner::class)
+            ->alias(StepsRunnerInterface::class, StepsRunner::class)
 
         ->set(TaskProgress::class)
             ->args([
                 new Reference(EntityManagerInterface::class),
             ])
-        ->alias(TaskProgressInterface::class, TaskProgress::class)
+            ->alias(TaskProgressInterface::class, TaskProgress::class)
     ;
 };
