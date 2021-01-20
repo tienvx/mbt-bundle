8b353c3944e6f3eafe161f89b4c41140cae9957a
tien.vo <tien.vo@sunbytes.vn>
2021-01-20 18:00:27 +0700

    Fix wrong translation domain

---
+++ src/Resources/translations/validators.en.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'mbt' => [
+        'model' => [
+            'places_invalid' => 'Places are invalid.',
+            'missing_start_places' => 'You must select at least 1 start place.',
+            'missing_from_places' => 'You must select at least 1 places to transition from.',
+            'missing_to_places' => 'You must select at least 1 places to transition to.',
+        ],
+    ],
+];
