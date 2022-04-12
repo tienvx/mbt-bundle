9c0b8fbeeed377282ea267ac54be45ff76f87a9c
tienvx <tien.xuan.vo@gmail.com>
2022-04-12 23:36:00 +0700

    More tests for CommandPreprocessor

--- tests/Command/CommandPreprocessorTest.php
+++ tests/Command/CommandPreprocessorTest.php
@@ -5,8 +5,11 @@ namespace Tienvx\Bundle\MbtBundle\Tests\Command;
 use PHPUnit\Framework\TestCase;
 use SingleColorPetrinet\Model\Color;
 use Tienvx\Bundle\MbtBundle\Command\CommandPreprocessor;
-use Tienvx\Bundle\MbtBundle\Command\Runner\AssertionRunner;
+use Tienvx\Bundle\MbtBundle\Command\Runner\KeyboardCommandRunner;
 use Tienvx\Bundle\MbtBundle\Command\Runner\MouseCommandRunner;
+use Tienvx\Bundle\MbtBundle\Command\Runner\ScriptCommandRunner;
+use Tienvx\Bundle\MbtBundle\Command\Runner\StoreCommandRunner;
+use Tienvx\Bundle\MbtBundle\Command\Runner\WindowCommandRunner;
 use Tienvx\Bundle\MbtBundle\Model\Model\Revision\Command;
 
 /**
@@ -16,39 +19,94 @@ use Tienvx\Bundle\MbtBundle\Model\Model\Revision\Command;
  */
 class CommandPreprocessorTest extends TestCase
 {
-    public function testProcess(): void
-    {
+    /**
+     * @dataProvider commandProvider
+     */
+    public function testProcess(
+        string $commandString,
+        ?string $target,
+        ?string $value,
+        array $values,
+        ?string $newTarget,
+        ?string $newValue
+    ): void {
         $command = new Command();
-        $command->setCommand(AssertionRunner::ASSERT);
-        $command->setTarget('${variable}');
-        $command->setValue('value');
+        $command->setCommand($commandString);
+        $command->setTarget($target);
+        $command->setValue($value);
         $color = new Color();
-        $color->setValues([
-            'variable' => 'key',
-            'key' => 'value',
-        ]);
+        $color->setValues($values);
         $preprocessor = new CommandPreprocessor();
         $newCommand = $preprocessor->process($command, $color);
-        $this->assertSame(AssertionRunner::ASSERT, $newCommand->getCommand());
-        $this->assertSame('key', $newCommand->getTarget());
-        $this->assertSame('value', $newCommand->getValue());
+        $this->assertSame($commandString, $newCommand->getCommand());
+        $this->assertSame($newTarget, $newCommand->getTarget());
+        $this->assertSame($newValue, $newCommand->getValue());
     }
 
-    public function testProcessEmpty(): void
+    public function commandProvider(): array
     {
-        $command = new Command();
-        $command->setCommand(MouseCommandRunner::CLICK);
-        $command->setTarget('');
-        $command->setValue(null);
-        $color = new Color();
-        $color->setValues([
-            'variable' => 'key',
-            'key' => 'value',
-        ]);
-        $preprocessor = new CommandPreprocessor();
-        $newCommand = $preprocessor->process($command, $color);
-        $this->assertSame(MouseCommandRunner::CLICK, $newCommand->getCommand());
-        $this->assertSame('', $newCommand->getTarget());
-        $this->assertSame(null, $newCommand->getValue());
+        return [
+            [
+                KeyboardCommandRunner::TYPE,
+                'xpath=//div[text()=\'${variable1}\']/input',
+                '${variable2}',
+                [
+                    'variable1' => 'value1',
+                    'variable2' => 'value2',
+                ],
+                'xpath=//div[text()=\'value1\']/input',
+                'value2',
+            ],
+            [
+                ScriptCommandRunner::EXECUTE_SCRIPT,
+                'return ${variable1} + 1',
+                'variable1',
+                [
+                    'variable1' => 123,
+                ],
+                'return 123 + 1',
+                'variable1',
+            ],
+            [
+                StoreCommandRunner::STORE,
+                'closed',
+                '${key}',
+                [
+                    'key' => 'status',
+                ],
+                'closed',
+                'status',
+            ],
+            [
+                WindowCommandRunner::OPEN,
+                'http://example.com/${path}',
+                null,
+                [],
+                'http://example.com/path',
+                null,
+            ],
+            [
+                MouseCommandRunner::CLICK,
+                'css=.id',
+                null,
+                [
+                    'key1' => 'value1',
+                    'key2' => 'value2',
+                ],
+                'css=.id',
+                null,
+            ],
+            [
+                'invalid',
+                '',
+                null,
+                [
+                    'key1' => 'value1',
+                    'key2' => 'value2',
+                ],
+                '',
+                null,
+            ],
+        ];
     }
 }
