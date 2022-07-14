3a423cdc5b716c93bc90a55bb10edd019b83b36a
tienvx <tien.xuan.vo@gmail.com>
2022-07-14 23:53:50 +0700

    Fix return type is not covariant

--- tests/Fixtures/Validator/CustomConstraintValidatorFactory.php
+++ tests/Fixtures/Validator/CustomConstraintValidatorFactory.php
@@ -4,6 +4,7 @@ namespace Tienvx\Bundle\MbtBundle\Tests\Fixtures\Validator;
 
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidatorFactory;
+use Symfony\Component\Validator\ConstraintValidatorInterface;
 use Tienvx\Bundle\MbtBundle\Command\CommandPreprocessor;
 use Tienvx\Bundle\MbtBundle\Command\CommandRunnerManager;
 use Tienvx\Bundle\MbtBundle\Command\Runner\AlertCommandRunner;
@@ -18,7 +19,7 @@ use Tienvx\Bundle\MbtBundle\Validator\ValidCommandValidator;
 
 class CustomConstraintValidatorFactory extends ConstraintValidatorFactory
 {
-    public function getInstance(Constraint $constraint)
+    public function getInstance(Constraint $constraint): ConstraintValidatorInterface
     {
         $className = $constraint->validatedBy();
         if (ValidCommandValidator::class === $className && !isset($this->validators[$className])) {
