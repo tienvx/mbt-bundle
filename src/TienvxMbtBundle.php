e06cebd08966be714a92a8a726b812e22d5aaf41
tienvx <tien.xuan.vo@gmail.com>
2022-07-19 00:33:25 +0700

    Use new bundle structure. Use new attributes from Symfony 6.1

--- src/TienvxMbtBundle.php
+++ src/TienvxMbtBundle.php
@@ -2,14 +2,41 @@
 
 namespace Tienvx\Bundle\MbtBundle;
 
+use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+use Tienvx\Bundle\MbtBundle\Command\Runner\CustomCommandRunner;
 use Tienvx\Bundle\MbtBundle\DependencyInjection\Compiler\PluginPass;
+use Tienvx\Bundle\MbtBundle\Service\SelenoidHelperInterface;
 
-class TienvxMbtBundle extends Bundle
+class TienvxMbtBundle extends AbstractBundle
 {
+    public const WEBDRIVER_URI = 'webdriver_uri';
+    public const UPLOAD_DIR = 'upload_dir';
+
     public function build(ContainerBuilder $container): void
     {
         $container->addCompilerPass(new PluginPass());
+    }
+
+    public function configure(DefinitionConfigurator $definition): void
+    {
+        $definition->import('../config/definition.php');
+    }
+
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $container->import('../config/services.php');
+
+        $container->services()
+            ->get(SelenoidHelperInterface::class)
+            ->call('setWebdriverUri', [$config[static::WEBDRIVER_URI]])
+        ;
+        $container->services()
+            ->get(CustomCommandRunner::class)
+            ->call('setWebdriverUri', [$config[static::WEBDRIVER_URI]])
+            ->call('setUploadDir', [$config[static::UPLOAD_DIR]])
+        ;
     }
 }
